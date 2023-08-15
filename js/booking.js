e07cdba33a4535557ed8d00d87b5e117373d5f1e
ashwin-saji <ashwinsajithoma2050@conestogac.on.ca>
2023-08-15 17:31:35 -0400

    booking.js code commited

--- js/booking.js
+++ js/booking.js
@@ -1,1 +1,14 @@
 "use strict";
+const $ = (selector, el=null) => {
+    if (!el) el = document;
+    return el.querySelector(selector);
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    $("#sub").addEventListener("click", (event)=>{
+        event.preventDefault();
+        window.location.href = "payment.html";
+    });
+});
